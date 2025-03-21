376f37768ee3f713d9055d5d63756718d3b3d54d
Ryuta Yamamoto <r.yamamoto0710@gmail.com>
2025-03-21 20:16:14 +0900

    Update a function in image.py: generate_lattice_sites
    Added a new option, ROI_origins, for origin position selection when generating lattice sites.
    Fix the code according to flake8.

--- qgm/image.py
+++ qgm/image.py
@@ -1,37 +1,34 @@
+import cv2
+import joblib
+import matplotlib.collections as mc
 import numpy as np
 import pandas as pd
-
-from numba import jit, uint16, float64, prange
-
-from PIL import Image
-import cv2
-
-import joblib
-
 # import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
-import matplotlib.collections as mc
+from numba import float64, jit, prange, uint16
+from PIL import Image
 
 from . import function
-from .parameter import system, psf, lattice
-from .function_fast import function_1d, function_2d
+# from .function_fast import function_1d, function_2d
+# from .parameter import lattice, psf, system
+from .parameter import system
+
 
 class image():
     def __init__(self, image_path=''):
         self.system = system()
-        
+
         if image_path != '':
             try:
                 self.image_path = image_path
-                # print(self.image_path)
                 self.load_image(self.image_path, remove_offset=False)
             except TypeError:
                 self.image_path = None
                 self.image = np.array([])
                 self.image_loaded = False
-        
+
         self.image_ROI = np.array([])
-        
+
         self.bg_image = np.array([])
         self.bg_image_loaded = False
         self.offset_removed = False
@@ -43,7 +40,7 @@ class image():
         self.ROI = {'Position (px)': pd.DataFrame([], columns=['x0 (px)', 'x1 (px)', 'y0 (px)', 'y1 (px)']),
                     'Position (um)': pd.DataFrame([], columns=['x0 (um)', 'x1 (um)', 'y0 (um)', 'y1 (um)']),
                     }
-        
+
     def load_image(self, path, remove_offset=False):
         self.image = np.array(Image.open(path), dtype=float)
         self.offset_removed = False
@@ -51,10 +48,10 @@ class image():
 
         if remove_offset:
             self.remove_offset()
-        
+
     def load_bg_image(self, path, remove_offset=True):
         self.bg_image = joblib.load(path)
-        
+
         if remove_offset:
             self.remove_offset()
             self.offset_removed = True
@@ -66,7 +63,7 @@ class image():
             print('Error: Background image is not prepared.')
 
     def generate_bg_image(self, paths, remove_offset=True):
-        if type(paths) == str:
+        if type(paths) is str:
             self.bg_image = np.array(Image.open(paths), dtype=float)
         else:
             for n, path in enumerate(paths):
@@ -80,7 +77,7 @@ class image():
         if remove_offset:
             self.remove_offset()
             self.offset_removed = True
-        
+
         self.bg_image_loaded = True
 
     def remove_offset(self):
@@ -101,19 +98,21 @@ class image():
         elif unit == 'px':
             lx = (np.arange(0, self.image.shape[1]) - x0)
             ly = (np.arange(0, self.image.shape[0]) - y0)
-        
+
         self.xymesh = np.meshgrid(lx, ly)
 
         return self.xymesh
 
-    def generate_lattice_sites(self, Nsite=2*10+1, rxlim=10, rylim=10, unit='um', limits='circle', origin='best'):
+    def generate_lattice_sites(self, Nsite=2*10+1, rxlim=10, rylim=10,
+                               unit='um', limits='circle', origin='best',
+                               ROI_origins=None):
         info_L1 = self.system.lattice['Lattice 1'].info
         info_L2 = self.system.lattice['Lattice 2'].info
-        
+
         if unit == 'um':
             a1 = info_L1['Constant (um)']
             a2 = info_L2['Constant (um)']
-            
+
             label_x0 = 'X Center (um)'
             label_y0 = 'Y Center (um)'
         elif unit == 'px':
@@ -125,7 +124,7 @@ class image():
 
         angle1 = info_L1['Angle (radian)']
         angle2 = info_L2['Angle (radian)']
-        
+
         a1x = a1 * np.cos(angle1)
         a1y = a1 * np.sin(angle1)
         a2x = a2 * np.cos(angle2)
@@ -133,22 +132,36 @@ class image():
 
         det = (a1x * a2y - a2x * a1y)
 
+        x0_all = self.system.lattice['Origins'][label_x0]
+        y0_all = self.system.lattice['Origins'][label_y0]
+
+        if ROI_origins is not None:
+            if len(ROI_origins) == 4:
+                x_min, x_max, y_min, y_max = ROI_origins
+
+                conditions = (x0_all <= x_max) * (x0_all >= x_min)
+                conditions *= (y0_all <= y_max) * (y0_all >= y_min)
+
+                if conditions.size >= 1:
+                    x0_all = x0_all[conditions]
+                    y0_all = y0_all[conditions]
+
         if origin == 'best':
             id_orig = np.argmax(self.system.lattice['Origins']['Goodness'])
 
-            x0 = self.system.lattice['Origins'][label_x0][id_orig]
-            y0 = self.system.lattice['Origins'][label_y0][id_orig]
+            x0 = x0_all[id_orig]
+            y0 = y0_all[id_orig]
 
             n01 = int((a2y * x0 - a2x * y0) / det)
             n02 = int((-a1y * x0 + a1x * y0) / det)
-            
+
             x0 = x0 - (n01 * a1x + n02 * a2x)
             y0 = y0 - (n01 * a1y + n02 * a2y)
         elif origin == 'average':
             x0s = []
             y0s = []
 
-            for x0_tmp, y0_tmp in zip(self.system.lattice['Origins'][label_x0], self.system.lattice['Origins'][label_y0]):
+            for x0_tmp, y0_tmp in zip(x0_all, y0_all):
                 n01 = int((a2y * x0_tmp - a2x * y0_tmp) / det)
                 n02 = int((-a1y * x0_tmp + a1x * y0_tmp) / det)
 
@@ -162,8 +175,8 @@ class image():
             x0s_comp = []
             y0s_comp = []
 
-            x0s_within = x0s[diff_sites<np.mean([a1,a2])/2]
-            y0s_within = y0s[diff_sites<np.mean([a1,a2])/2]
+            x0s_within = x0s[diff_sites < np.mean([a1, a2])/2]
+            y0s_within = y0s[diff_sites < np.mean([a1, a2])/2]
 
             x0_comp_origin = np.mean(x0s_within)
             y0_comp_origin = np.mean(y0s_within)
@@ -171,7 +184,7 @@ class image():
             n = [-1, 0, 1]
             n1, n2 = np.meshgrid(n, n)
 
-            for x0_tmp, y0_tmp in zip(x0s[diff_sites>=np.mean([a1,a2])/2], y0s[diff_sites>=np.mean([a1,a2])/2]):
+            for x0_tmp, y0_tmp in zip(x0s[diff_sites >= np.mean([a1, a2])/2], y0s[diff_sites >= np.mean([a1, a2])/2]):
                 x0_recalc = np.reshape(x0_tmp + (n1 * a1x + n2 * a2x), n1.size)
                 y0_recalc = np.reshape(y0_tmp + (n1 * a1y + n2 * a2y), n1.size)
                 diff_center = np.reshape(np.sqrt((x0_recalc - x0_comp_origin)**2 + (y0_recalc - y0_comp_origin)**2), n1.size)
@@ -185,19 +198,19 @@ class image():
             x0 = np.mean(x0s)
             y0 = np.mean(y0s)
         else:
-            if type(origin) == int:
+            if type(origin) is int:
                 x0 = self.system.lattice['Origins'][label_x0][origin]
                 y0 = self.system.lattice['Origins'][label_y0][origin]
 
                 n01 = int((a2y * x0 - a2x * y0) / det)
                 n02 = int((-a1y * x0 + a1x * y0) / det)
-                
+
                 x0 = x0 - (n01 * a1x + n02 * a2x)
                 y0 = y0 - (n01 * a1y + n02 * a2y)
 
         n = [[i - (Nsite-1)/2] for i in range(Nsite)]
         n1, n2 = np.meshgrid(n, n)
-        
+
         xs_all = n1 * a1 * np.cos(angle1) + n2 * a2 * np.cos(angle2) + x0
         ys_all = n1 * a1 * np.sin(angle1) + n2 * a2 * np.sin(angle2) + y0
 
@@ -212,27 +225,27 @@ class image():
         else:
             xs = xs_all
             ys = ys_all
-        
+
         self.system.lattice['Lattice sites'] = pd.DataFrame([], columns=['X Center', 'Y Center', 'Amplitude'])
         self.system.lattice['Lattice sites']['X Center'] = xs
         self.system.lattice['Lattice sites']['Y Center'] = ys
 
         return xs, ys
-    
+
     def generate_psfm(self, psf_model=None):
         x0s = np.array(self.system.lattice['Lattice sites']['X Center'], dtype=float)
         y0s = np.array(self.system.lattice['Lattice sites']['Y Center'], dtype=float)
 
         psfm_size = np.array((self.xymesh_ROI[0].shape[0], self.xymesh_ROI[0].shape[1], x0s.size), dtype=np.uint16)
-        
-        if psf_model == None:
+
+        if psf_model is None:
             psf_model = self.system.psf.info['Model']
 
         if psf_model == 'psf':
             self.psfm = np.zeros([self.image_ROI.shape[0], self.image_ROI.shape[1], len(self.system.lattice['Lattice sites'])])
 
             alpha0 = 2 * np.pi / self.system.psf.info['Wavelength (um)'] * self.system.psf.info['Effective NA']
-            
+
             # for ind_item, row in self.system.lattice['Lattice sites'].iterrows():
             for ind_item, (x0, y0) in enumerate(zip(x0s, y0s)):
                 # x0 = row['X Center']
@@ -244,12 +257,12 @@ class image():
         elif psf_model == 'gaussian':
             sigmax = self.system.psf.info['HWHM width - x (um)'] / np.sqrt(np.log(2))
             sigmay = self.system.psf.info['HWHM width - y (um)'] / np.sqrt(np.log(2))
-            
+
             self.psfm = generate_gauss_psfm(psfm_size, self.xymesh_ROI[0], self.xymesh_ROI[1],
                                             x0s, y0s, np.array([sigmax, sigmay], dtype=np.float64))
 
             # self.psfm = np.zeros([self.image_ROI.shape[0], self.image_ROI.shape[1], len(self.system.lattice['Lattice sites'])])
-            
+
             # for ind_item, (x0, y0) in enumerate(zip(x0s, y0s)):
             #     # x0 = row['X Center']
             #     # y0 = row['Y Center']
@@ -258,7 +271,7 @@ class image():
             #     self.psfm[:, :, ind_item] = function.gaussian_2d(self.xymesh_ROI, *p_gaussian)
 
     def set_ROI(self, ROI=None, unit='um'):
-        if ROI == None:
+        if ROI is None:
             ROI_px = {'x0 (px)': [0], 'x1 (px)': [self.image.shape[1]-1], 'y0 (px)': [0], 'y1 (px)': [self.image.shape[0]-1]}
             ROI_um = {'x0 (um)': [np.min(self.xymesh[0])], 'x1 (um)': [np.max(self.xymesh[0])],
                       'y0 (um)': [np.min(self.xymesh[1])], 'y1 (um)': [np.max(self.xymesh[1])]}
@@ -281,7 +294,8 @@ class image():
             id_y0 = np.argmin(np.abs(yy.T-y0))
             id_y1 = np.argmin(np.abs(yy.T-y1))
 
-            ROI_px = {'x0 (px)': [id_x0], 'x1 (px)': [id_x1], 'y0 (px)': [id_y0], 'y1 (px)': [id_y1]}
+            ROI_px = {'x0 (px)': [id_x0], 'x1 (px)': [id_x1],
+                      'y0 (px)': [id_y0], 'y1 (px)': [id_y1]}
             ROI_um = {'x0 (um)': [xx[0, id_x0]], 'x1 (um)': [xx[0, id_x1]],
                       'y0 (um)': [yy[id_y0, 0]], 'y1 (um)': [yy[id_y1, 0]]}
 
@@ -292,31 +306,31 @@ class image():
             self.xymesh_ROI = (xx[id_y0:id_y1, id_x0:id_x1], yy[id_y0:id_y1, id_x0:id_x1])
 
     def generate_lattice_separation(self, color='w', alpha=0.75, dx=0, dy=0):
-        dpx = self.system.info['Effective Pixel size (um/px)']
-        
+        # dpx = self.system.info['Effective Pixel size (um/px)']
+
         a1um = self.system.lattice['Lattice 1'].info['Constant (um)']
         a2um = self.system.lattice['Lattice 2'].info['Constant (um)']
         theta1 = self.system.lattice['Lattice 1'].info['Angle (radian)']
         theta2 = self.system.lattice['Lattice 2'].info['Angle (radian)']
-        
+
         a1vec = np.array([a1um * np.cos(theta1), a1um * np.sin(theta1)])
         a2vec = np.array([a2um * np.cos(theta2), a2um * np.sin(theta2)])
         a3vec = a1vec - a2vec
-        
+
         xs = np.array(self.system.lattice['Lattice sites']['X Center'])
         ys = np.array(self.system.lattice['Lattice sites']['Y Center'])
-        
+
         lseps = []
-        
+
         for k in range(xs.size):
             p0 = (xs[k]+dx, ys[k]+dy)
 
             lseps += [[p0 + (2*a2vec + a3vec)/3, p0 + (2*a1vec - a2vec)/3,
-                    p0 + (2*a3vec - a1vec)/3, p0 + (-2*a2vec - a3vec)/3,
-                    p0 + (-2*a1vec + a2vec)/3, p0 + (-2*a3vec + a1vec)/3,
-                    p0 + (2*a2vec + a3vec)/3,
-                    ]]
-        
+                       p0 + (2*a3vec - a1vec)/3, p0 + (-2*a2vec - a3vec)/3,
+                       p0 + (-2*a1vec + a2vec)/3, p0 + (-2*a3vec + a1vec)/3,
+                       p0 + (2*a2vec + a3vec)/3,
+                       ]]
+
         sep_lines = mc.LineCollection(lseps, colors=color, alpha=alpha)
 
         return sep_lines
@@ -324,20 +338,21 @@ class image():
     def show_image(self):
         pass
 
-@jit(float64[:,:,:](uint16[:], float64[:,:], float64[:,:], float64[:], float64[:], float64[:]))
-def generate_gauss_psfm(size:np.ndarray, x: np.ndarray, y: np.ndarray,
-                      x0s:np.ndarray, y0s:np.ndarray, sigmas: np.ndarray) -> np.ndarray:
+@jit(float64[:, :, :](uint16[:], float64[:, :], float64[:, :], float64[:], float64[:], float64[:]))
+def generate_gauss_psfm(size: np.ndarray, x: np.ndarray, y: np.ndarray,
+                        x0s: np.ndarray, y0s: np.ndarray, sigmas: np.ndarray) -> np.ndarray:
     psfm = np.zeros((size[0], size[1], size[2]), dtype=np.float64)
-    
+
     for n in prange(size[2]):
         for k in range(size[0]):
             for l in range(size[1]):
-                xs = x[k,l] - x0s[n]
-                ys = y[k,l] - y0s[n]
-                
-                psfm[k,l,n] = np.exp(-xs**2/sigmas[0]**2-ys**2/sigmas[1]**2)
-    
+                xs = x[k, l] - x0s[n]
+                ys = y[k, l] - y0s[n]
+
+                psfm[k, l, n] = np.exp(-xs**2/sigmas[0]**2-ys**2/sigmas[1]**2)
+
     return psfm
+
 
 def generate_cmap(colors):
     """ Generate user defined colormap
@@ -346,7 +361,7 @@ def generate_cmap(colors):
     ----------
     colors : list
         [description]
-    
+
     Returns
     -------
     [type]
@@ -355,11 +370,12 @@ def generate_cmap(colors):
     values = range(len(colors))
     vmax = np.ceil(np.max(values))
     color_list = []
-    
+
     for v, c in zip(values, colors):
-        color_list.append( ( v / vmax, c) )
-    
+        color_list.append((v / vmax, c))
+
     return LinearSegmentedColormap.from_list('custom', color_list)
+
 
 def pixel_shift(image, dx, dy, interporation='Lanczos'):
     matrix = [[1, 0, dx], [0, 1, dy]]
@@ -367,6 +383,5 @@ def pixel_shift(image, dx, dy, interporation='Lanczos'):
 
     image_shifted = cv2.warpAffine(image, affine_matrix_fit, image.shape,
                                    flags=cv2.INTER_LANCZOS4)
-    
-    return image_shifted
 
+    return image_shifted
